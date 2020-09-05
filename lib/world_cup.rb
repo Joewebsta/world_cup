77119b35d9b3d9fdce3a7d56bde2182492ba53b8
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-05 08:50:18 -0400

    Refactor WorldCup#all_players_by_position

--- lib/world_cup.rb
+++ lib/world_cup.rb
@@ -10,23 +10,8 @@ class WorldCup
     alive_teams = teams.find_all { |team| team.eliminated? == false }
     alive_teams.flat_map { |team| team.players_by_position(position) }
   end
+
+  def all_players_by_position
+    teams.flat_map(&:players).group_by(&:position)
+  end
 end
-
-# require './lib/player'
-# require './lib/team'
-
-# france = Team.new('France')
-# mbappe = Player.new({ name: 'Kylian Mbappe', position: 'forward' })
-# pogba = Player.new({ name: 'Paul Pogba', position: 'midfielder' })
-# france.add_player(mbappe)
-# france.add_player(pogba)
-
-# croatia = Team.new('Croatia')
-# modric = Player.new({ name: 'Luka Modric', position: 'midfielder' })
-# vida = Player.new({ name: 'Domagoj Vida', position: 'defender' })
-# croatia.add_player(modric)
-# croatia.add_player(vida)
-# croatia.eliminated = true
-
-# world_cup = WorldCup.new(2018, france, croatia)
-# pp world_cup.active_players_by_position('midfielder')
